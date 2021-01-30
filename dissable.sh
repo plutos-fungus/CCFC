99ada8076e4feababf9c60128ed787eacceee943
plutos-fungus <rasmus1070@gmail.com>
2021-01-30 16:02:23 +0100

    Making them actually work

--- dissable.sh
+++ dissable.sh
@@ -1,4 +1,4 @@
-sudo echo 0 > /sys/devices/system/cpu/cpu1/online
-sudo echo 0 > /sys/devices/system/cpu/cpu2/online
-sudo echo 0 > /sys/devices/system/cpu/cpu3/online
-sudo echo 0 > /sys/devices/system/cpu/cpu4/online
+echo 0 > /sys/devices/system/cpu/cpu1/online
+echo 0 > /sys/devices/system/cpu/cpu2/online
+echo 0 > /sys/devices/system/cpu/cpu3/online
+echo 0 > /sys/devices/system/cpu/cpu4/online
