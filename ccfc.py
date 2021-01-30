4aaf554753fc951ce12e289a65ac65429768298c
plutos-fungus <rasmus1070@gmail.com>
2021-01-30 13:50:59 +0100

    Quality of life update

--- ccfc.py
+++ ccfc.py
@@ -6,6 +6,15 @@ inp = ''
 #Just a loop
 while F == 0:
     inp = input('> ')
+    print("Commands:")
+    print("slow  >   Makes the CPU to utilize a slower frequency")
+    print("fast  >   Makes the CPU to utilize a faster frequency")
+    print("dis   >   Dissables 4 CPU cores")
+    print("ena   >   Enables the 4 dissabled cores")
+    print("check >   Checks the CPU frequency for every CPU thread")
+    print("start >   Starts the Java Paper Spigot Minecraft server")
+    print("q     >   Quits the Python script")
+
     #Set CPU frequency to slow
     if inp == 'slow':
         #Min frequency
@@ -18,14 +27,17 @@ while F == 0:
         os.system("sudo cpupower frequency-set -d 1.5GHz")
         #Max frequency
         os.system("sudo cpupower frequency-set -u 3GHz")
+    '''
     #Dissable 4 CPU cores
     #Needs sudo su for some reason :(
+    #doesn't work right now
     elif inp == 'dis':
         os.system("./dissable.sh")
     #Enables 4 CPU cores
     #Also needs sudo su for some reason :(
     elif inp == 'ena':
         os.system("./enable.sh")
+        '''
     #Check the CPU frequency
     elif inp == 'check':
         os.system("cat /proc/cpuinfo | grep MHz")
