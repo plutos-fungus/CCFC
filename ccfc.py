0781ff5ebbfdc11f595e0847cda5569fb396650f
plutos-fungus <rasmus1070@gmail.com>
2021-01-31 15:10:52 +0100

    It should work now

--- ccfc.py
+++ ccfc.py
@@ -20,31 +20,31 @@ while F == 0:
 
 
     #Set CPU frequency to slow
-    if inp == "slow":
+    if inp == "slow" or inp == "s":
         #Min frequency
         os.system("sudo cpupower frequency-set -d 800MHz")
         #Max frequency
         os.system("sudo cpupower frequency-set -u 800MHz")
 
     #Set CPU frequency to fast
-    elif inp == "fast":
+    elif inp == "fast" or inp == "f":
         #Min frequency
         os.system("sudo cpupower frequency-set -d 2GHz")
         #Max frequency
         os.system("sudo cpupower frequency-set -u 3.8GHz")
 
     #Dissable 4 CPU cores
-    elif inp == "dis":
+    elif inp == "dis" or inp == "d":
         subprocess.call(['sh', './dissable.sh'])
         #os.system("./dissable.sh")
 
     #Enables 4 CPU cores
-    elif inp == "ena":
+    elif inp == "ena" or inp == "e":
         subprocess.call(['sh', './enable.sh'])
         #os.system("./enable.sh")
 
     #Check the CPU frequency
-    elif inp == "check":
+    elif inp == "check" or inp == "c":
         #Frequency pr. CPU thread
         print("Frequency pr. CPU thread")
         os.system("cat /proc/cpuinfo | grep MHz")
@@ -54,11 +54,11 @@ while F == 0:
 
     #Start the server
     #The java file has to be in the same folder as this script
-    elif inp == "start":
+    elif inp == "start" or inp == "ss":
         subprocess.call(['sh', './run.sh'])
         #os.system("./run.sh")
 
-    elif inp == "help":
+    elif inp == "help" or inp == "h":
         print("Commands:")
         print("s or slow  >   Makes the CPU to utilize a slower frequency")
         print("f or fast  >   Makes the CPU to utilize a faster frequency")
